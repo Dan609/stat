43055f83ceed641eaa0549803497e0652d2d4978
Dan Bobkov <dan.bobkov@gmail.com>
2019-05-14 17:33:38 +0300

    .

--- traj_function.R
+++ traj_function.R
@@ -1,14 +1,38 @@
-library("trajr")
-
+library(trajr)
+library(tibble)
+library(ggplot2)
+library(plyr)
+library(dplyr) 
+library(ggpubr)
+library(car)
+library(stringi)
+library(Hmisc)
+library(gplots)
+library(PMCMRplus)
+library(dunn.test)
+library(DescTools)
+library(ggsignif)
 
 ##### Track analysis function ###
 
-
 traj_analysis <- function(input) {
   
-  data <- input
+  data <- read.csv(input)
+  
+  traj_params <- setNames(data.frame(matrix(ncol = 10, nrow = 0)), 
+                          c("track", 
+                            "lenght",
+                            "distance",
+                            "square_displacement",
+                            "mean_speed",
+                            "sd_speed",
+                            "max_speed",
+                            "min_speed",
+                            "sinuosity",
+                            "emax"))
   
   for (i in unique(data$Track)) {
+
     # Define x, y, and time coordinates
     coords <- data.frame(x = data$X[data$Track == i], 
                          y = data$Y[data$Track == i], 
@@ -20,56 +44,43 @@ traj_analysis <- function(input) {
     trj <- TrajScale(trj, 1.3158 / 1, "£gm")
     
     # Plot it
-    # plot(trj, lwd = 2)
+    plot(trj, lwd = 2)
     
     # Trajectory analysis
     resampled <- TrajRediscretize(trj, 3)
     # The TrajDerivatives function calculates linear speed and acceleration along a Trajectory
     derivs <- TrajDerivatives(trj)
-    # total length of the trajectory
-    TrajLength(trj)
-    # straight-line distance from the start to the end of the trajectory
-    TrajDistance(trj)
-    # expected square displacement of a correlated random walk
-    TrajExpectedSquareDisplacement(trj)
-    # Measures of speed
-    mean(derivs$speed)
-    sd(derivs$speed)
-    max(derivs$speed)
-    min(derivs$speed)
-    # Measures of straightness
-    TrajSinuosity(trj)
-    TrajEmax(resampled)
     
-    
-    # Return a list with all of the statistics for this trajectory
-    traj_params <- list(lenght = TrajLength(trj),
-                        distance = TrajDistance(trj),
-                        square_displacement = TrajExpectedSquareDisplacement(trj),
-                        mean_speed = mean(derivs$speed),
-                        sd_speed = sd(derivs$speed),
-                        max_speed = max(derivs$speed),
-                        min_speed = min(derivs$speed),
-                        sinuosity = TrajSinuosity(trj),
-                        emax = TrajEmax(resampled)
-    )
-    
-    
-    return(traj_params)
-    
+    traj_params <- add_row(traj_params, 
+            track = i,
+            # total length of the trajectory
+            lenght = TrajLength(trj),
+            # straight-line distance from the start to the end of the trajectory
+            distance = TrajDistance(trj),
+            # expected square displacement of a correlated random walk
+            square_displacement = TrajExpectedSquareDisplacement(trj),
+            # Measures of speed
+            mean_speed = mean(derivs$speed),
+            sd_speed = sd(derivs$speed),
+            max_speed = max(derivs$speed),
+            min_speed = min(derivs$speed),
+            # Measures of straightness
+            sinuosity = TrajSinuosity(trj),
+            emax = TrajEmax(resampled)
+            )
     
   }
+
+  # print(traj_params)
+  write.csv(traj_params, file = 'track.csv')
+  a <<- traj_params
+  return(traj_params)
   
-  tracks <- as.data.frame(rbind(
-    traj_params,
-    traj_params
-  ), stringsAsFactors = FALSE)
-  
-  return(tracks)
 }
 
 
+# call 
+traj_analysis('F0001.csv')
 
 
-
-traj_analysis(read.csv('F0001.csv'))
+scatterplot(a$track, a$length)
